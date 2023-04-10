1f9dedd9899aea7cd3dbc84ed41b3bcc3f95900b
syahrulirwansyah12 <ach.syahrul99@gmail.com>
2023-04-11 08:47:34 +0900

    Initial commit

---
+++ shutdown.py
@@ -0,0 +1,36 @@
+"""
+#!/usr/bin/env python
+#title           :shutdown.py
+#description     :Python Script read Raspberry Pi GPIO pin (digital input)
+#author          :Achmad Syahrul Irwansyah
+#date            :2023/03/30
+#version         :0.1
+#usage           :Python
+#notes           :
+#python_version  :2.7.17
+#==============================================================================
+"""
+
+import sys
+import RPi.GPIO as GPIO
+import subprocess as run
+
+PASSWORD = sys.argv[1]
+
+GPIO_PIN_S = 27
+GPIO_PIN_A = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN_S, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(GPIO_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    if GPIO.input(GPIO_PIN_A) == GPIO.LOW:
+        if GPIO.input(GPIO_PIN_S) == GPIO.HIGH:
+            print("Shutdown!!")
+            run.call("echo '" + PASSWORD + "' | sudo -S shutdown -h now", shell=True)
+            #run.call("echo '" + PASSWORD + "' | sudo -S shutdown -r now", shell=True)
+        else:
+            print("System Power ON.")
+    else:
+        print("Connect the wire!")
